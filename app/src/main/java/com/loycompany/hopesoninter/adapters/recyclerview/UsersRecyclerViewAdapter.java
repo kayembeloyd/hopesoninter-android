343f6c5a14e2c52c8eac1b02b79b1e2f55a9205c
Lloyd Kayembe <kayembeloyd@gmail.com>
2022-08-04 06:17:48 +0200

    Adding members phase 2

--- app/src/main/java/com/loycompany/hopesoninter/adapters/recyclerview/UsersRecyclerViewAdapter.java
+++ app/src/main/java/com/loycompany/hopesoninter/adapters/recyclerview/UsersRecyclerViewAdapter.java
@@ -5,17 +5,24 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.loycompany.hopesoninter.CommunityMemberViewActivity;
 import com.loycompany.hopesoninter.R;
+import com.loycompany.hopesoninter.authentication.Authenticator;
 import com.loycompany.hopesoninter.classes.User;
+import com.loycompany.hopesoninter.network.ServerRequest;
+import com.loycompany.hopesoninter.network.URLs;
 
 import java.util.List;
+import java.util.Objects;
 
 public class UsersRecyclerViewAdapter extends RecyclerView.Adapter<UsersRecyclerViewAdapter.ViewHolder>{
 
@@ -41,6 +48,54 @@ public class UsersRecyclerViewAdapter extends RecyclerView.Adapter<UsersRecycler
                 mContext.startActivity(intent);
             }
         });
+
+        if (users.get(position).requestingMembershipCommunityID == 0){
+            holder.requestingMembershipOptionsLinearLayout.setVisibility(View.GONE);
+            holder.requestingMembershipTextView.setVisibility(View.GONE);
+        } else {
+
+            if (Authenticator.getAuthUser(mContext).access.equals("admin")){
+                holder.requestingMembershipOptionsLinearLayout.setVisibility(View.VISIBLE);
+            } else if (Authenticator.getAuthUser(mContext).access.equals("community_leader")){
+                if (Authenticator.getAuthUser(mContext).communityID == users.get(position).requestingMembershipCommunityID){
+                    holder.requestingMembershipOptionsLinearLayout.setVisibility(View.VISIBLE);
+                }
+            } else {
+                holder.requestingMembershipOptionsLinearLayout.setVisibility(View.GONE);
+            }
+
+            holder.requestingMembershipTextView.setVisibility(View.VISIBLE);
+
+            holder.acceptButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Accept
+                    ServerRequest serverRequest = new ServerRequest(mContext) {
+                        @Override
+                        public void onServerResponse(String response) {
+                            Toast.makeText(mContext, "Server has responded: " + response, Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onServerError(String error) {
+                            Toast.makeText(mContext, "Error: " + error, Toast.LENGTH_SHORT).show();
+                        }
+                    };
+
+                    serverRequest.addHeaderParam("Accept", "application/json");
+                    serverRequest.addHeaderParam("Authorization",
+                            "Bearer " + Objects.requireNonNull(
+                                    Authenticator.getAuthUser(mContext)).getToken());
+
+                    serverRequest.sendRequest(URLs.getApiAddress2() +
+                            "/communities/" +
+                            users.get(position).requestingMembershipCommunityID +
+                            "/users/" +
+                            users.get(position).id +
+                            "/accept-membership", Request.Method.PUT);
+                }
+            });
+        }
     }
 
     @NonNull
@@ -53,12 +108,18 @@ public class UsersRecyclerViewAdapter extends RecyclerView.Adapter<UsersRecycler
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView nameTextView;
-        LinearLayout userLinearLayout;
+        TextView nameTextView, requestingMembershipTextView;
+        LinearLayout userLinearLayout, requestingMembershipOptionsLinearLayout;
+        Button acceptButton;
         public ViewHolder(View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.name_text_view);
             userLinearLayout = itemView.findViewById(R.id.user_linear_layout);
+
+            requestingMembershipTextView = itemView.findViewById(R.id.requesting_membership_textview);
+            requestingMembershipOptionsLinearLayout = itemView.findViewById(R.id.requesting_membership_options_linear_layout);
+
+            acceptButton = itemView.findViewById(R.id.accept_button);
         }
     }
 
